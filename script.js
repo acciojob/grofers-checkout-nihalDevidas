75e97a3c3c74527018d025c9f712d4d247b6bcaa
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-08-29 14:28:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	const ele = document.querySelectorAll(".price");
+
+	let sum=0;
+	ele.forEach((el)=>{
+		let price = el.innerText;
+		let price1 = parseInt(price);
+		sum = sum +price1;
+	});
+  const con = document.createElement("tr");
+  const td1 = document.createElement("td");
+	const td2 = document.createElement("td");
+	td1.innerText = "Total:";
+	td2.innerText  = `${sum}`;
+	con.append(td1,td2);
+
+	document.getElementById('grof').append(con);
 };
 
 getSumBtn.addEventListener("click", getSum);
