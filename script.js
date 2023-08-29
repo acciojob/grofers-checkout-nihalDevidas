796a4051d30caa81cd923e168cfae344e93e1d25
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-08-29 14:34:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const getSum = () => {
   const con = document.createElement("tr");
   const td1 = document.createElement("td");
 	const td2 = document.createElement("td");
+	td2.id = "ans";
 	td1.innerText = "Total:";
 	td2.innerText  = `${sum}`;
 	con.append(td1,td2);
